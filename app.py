0280a05f72a0a55416af17e6699623ef634d7e27
danipetran <87333685+danipetran@users.noreply.github.com>
2026-05-02 14:27:46 -0300

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ def conectar_planilha():
 st.set_page_config(page_title="Anamnese - Dani", page_icon="📋")
 
 st.title("📋 Anamnese Nutricional")
-st.markdown("Preencha com atenção para seu plano personalizado 💛")
+st.markdown("Preencha com atenção para seu plano personalizado ATUAL 💛")
 
 # ------------------------
 # DADOS PESSOAIS
